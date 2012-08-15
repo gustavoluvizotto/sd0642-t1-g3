540665f1e6780d190d4cc695e4c5e92c682aa043
gustavoluvizotto <gustavoluvizotto@d3a8180a-2837-12e8-d62d-ef69add598e3>
2012-08-15 03:21:30 +0000

    Codigo Cliente1 com comentarios e servidor1 apenas no inicio

--- client1.c
+++ client1.c
@@ -3,15 +3,16 @@
 #include <time.h>
 #include <string.h>
 #include <unistd.h>     /* used for read and write sys call */
-#include <sys/types.h>  /* used for socket sys call*/
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 
-#define MAXX 10000
-#define TAM 125
+#define MAXX 10000	/* to generate rand numbers betwen 0 to 9999 */
+#define TAM 125		/* size of rand vector */
 
 /*
+* sequence of socket connection for client
 * int socket(int domain, int type, int protocol);
 * int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 * ssize_t read(int fd, void *buf, size_t count);
@@ -19,38 +20,38 @@
 */
 
 int main(void){
-        int v[TAM];
-        int i, sockfd, port_number;
-        char *host;
-        struct sockaddr_in serv_addr;
-        struct hostent *server;
+        int v[TAM];				/* rand vector to future sort */
+        int i, sockfd, port_number;		/* index; file descriptor; port number of connection */
+        char *host;				/* name of server host */
+        struct sockaddr_in serv_addr;	/* structure containing an internet address */
+        struct hostent *server;		/* defines a host computer on the Internet */
 
         port_number = 57531;    /* port communication */
         host = "venon";         /* host who client 1 will communicate */
 
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);	/* socket communication using Internet */
         if (sockfd < 0) {
                 error("ERROR opening socket");
                 exit(1);
         }
-        server = gethostbyname(host);
+        server = gethostbyname(host);	/* return a pointer to host of a line in the network host data base */
         if (server == NULL) {
                 fprintf(stderr,"ERROR, no such host\n");
                 exit(0);
         }
-/*      serv_addr.sin_family = AF_INET;
-        //bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-        serv_addr.sin_port = htons(port_number);        
-        if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-*/
+//      serv_addr.sin_family = AF_INET;	/* family must be AF_INET */
+//      bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length); /* copies length bytes from arg1 to arg2 */
+//      serv_addr.sin_port = htons(port_number); /* converts a port number in host byte order to a port number in network byte order */       
+//      if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)	/* establish a connection to the server */
+//      	error("ERROR connecting");
+
         bzero(v,TAM);
         srand(time(NULL));
         for (i=0; i<TAM; i++)
-                v[i] = rand()%MAXX;     //rand number between 0 to 999
+                v[i] = rand()%MAXX;     /* rand number between 0 to 9999 */
 
-        bzero(v,TAM);
-        if (write(sockfd,v,TAM) < 0)
+        //bzero(v,TAM);
+        if (write(sockfd,v,TAM) < 0)	/* write vector at the socket */
                 error("Error writing to socket");
 
         close(sockfd);
