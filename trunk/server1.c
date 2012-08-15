8c85fa2283724fe9b003e76d395a3513a5b87fc3
gustavoluvizotto <gustavoluvizotto@d3a8180a-2837-12e8-d62d-ef69add598e3>
2012-08-15 20:02:41 +0000

    Client 1 e 2 e servidor 1. Servidor 1 une os dois vetores dos dois clientes e ordena-os. Falta agora fazer o segundo nivel cliente/servidor

--- trunk/server1.c
+++ trunk/server1.c
@@ -7,7 +7,21 @@
 #include <netinet/in.h>
 
 #define TAM 125
+/*Bubble sort*/
+void bubble(int v[], int qtd) {
+	int i, j, aux, k = qtd-1;
 
+	for(i = 0; i < qtd; i++) {
+		for(j = 0; j < k; j++) {
+			if(v[j] > v[j+1]) {
+				aux = v[j];
+				v[j] = v[j+1];
+				v[j+1]=aux;
+			}
+		}
+		k--;
+	}
+}
 /*
 * sequence of socket connection for server 
 * int socket(int domain, int type, int protocol);
@@ -18,11 +32,12 @@
 
 int main(void) {
        int port_number, sockfd, newsockfd;		/* port number and file descriptors */
-       struct sockaddr_in serv_addr, cli_addr; 	/* structure containing an internet address */
-       socklen_t clilen;				/* address of the client */
-	int v1[TAM], v2[TAM];				/* vectors sended from clients*/ 
+       struct sockaddr_in serv_addr, cli_addr; 		/* structure containing an internet address */
+       socklen_t clilen;					/* address of the client */
+	int v1[TAM], v2[TAM];				/* vectors sended from clients*/
+	int v3[2*TAM];					/* sorted vector */ 
 	char *aux1[TAM], *aux2[TAM];			/* receive vectors */        
-	int n;						/* if read and write OK */
+	int n;							/* if read and write OK */
 	int i;
 	
        port_number = 57531;                           /* port communication */
@@ -35,9 +50,10 @@ int main(void) {
         
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) /* binds a socket to an address */
                 error("ERROR on binding");
-        listen(sockfd,5);	/* process to listen communication. Arg2 is the number of connections that can be waiting while the process is handling a particular connection */
-     	
+       listen(sockfd,5);	/* process to listen communication. Arg2 is the number of connections that can be waiting while the process is handling a particular connection */
 	clilen = sizeof(cli_addr);
+	
+/*------------COMMUNICATING WITH THE FIRST CLIENT--------------------------------------*/
      	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen); /* block process until client connect. Wake up when client is on. Return new socket to communicate */
      	if (newsockfd < 0) 
           	error("ERROR on accept");
@@ -49,12 +65,34 @@ int main(void) {
      		if (n < 0) 
 			error("ERROR reading from socket");
 	}
-
-	for (i=0; i<TAM; i++) {
+	for (i=0; i<TAM; i++)
 		v1[i] = atoi(aux1[i]);
-		printf("%d - %d\n",i+1,v1[i]);
-	}
+/*--------------------------------------------------------------------------------------*/
 
+/*------------COMMUNICATING WITH THE SECOND CLIENT--------------------------------------*/
+	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen); /* block process until client connect. Wake up when client is on. Return new socket to communicate */
+     	if (newsockfd < 0) 
+          	error("ERROR on accept");
+	for (i=0; i<TAM; i++) {
+		aux2[i] = (char *) malloc (sizeof(char));
+		bzero(aux2[i],5);
+		n = read(newsockfd,aux2[i],5);
+     		if (n < 0) 
+			error("ERROR reading from socket");
+	}
+	for (i=0; i<TAM; i++) 
+		v2[i] = atoi(aux2[i]);
+/*--------------------------------------------------------------------------------------*/
+
+/*---------- copying the two clients vector to v3 and sort it --------------------------*/
+	for (i=0; i<TAM; i++)
+		v3[i] = v1[i];
+	for (i=TAM; i<2*TAM; i++)
+		v3[i] = v2[i-TAM];
+	bubble(v3,2*TAM);
+	for (i=0; i<2*TAM; i++)
+		printf("%d - %d\n",i+1,v3[i]);
+/*--------------------------------------------------------------------------------------*/	
 	close(newsockfd);
 	close(sockfd);
        return 0;
