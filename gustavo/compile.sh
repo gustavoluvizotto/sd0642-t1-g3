bbaffabd8c1295e6922fc31cdcf89db3537dd505
gustavoluvizotto@gmail.com <gustavoluvizotto@gmail.com@d3a8180a-2837-12e8-d62d-ef69add598e3>
2012-09-09 13:35:42 +0000

    Script para compilar todos os outros códigos postados

---
+++ gustavo/compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+gcc -o client1 client1.c
+gcc -o client2 client2.c
+gcc -o client3 client3.c
+gcc -o client4 client4.c
+gcc -o client5 client5.c
+gcc -o client6 client6.c
+gcc -o client7 client7.c
+gcc -o client8 client8.c
+gcc -o server1 server1.c
+gcc -o server2 server2.c
+gcc -o server3 server3.c
+gcc -o server4 server4.c
+gcc -o servers1 servers1.c
+gcc -o servers2 servers2.c
+gcc -o serverp serverp.c
