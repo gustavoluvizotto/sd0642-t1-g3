69ad616d977621344a1593bc0f73a9dd8e6026fe
lcrespilho@gmail.com <lcrespilho@gmail.com@d3a8180a-2837-12e8-d62d-ef69add598e3>
2012-09-09 22:33:29 +0000

    Organizando a bagunça...

--- leo/v3.0/error.c
+++
@@ -1,9 +0,0 @@
-#include "tp1.h"
-
-/* quando uma função gera um erro e seta a variável global errno,
- * esta função imprime o erro e aborta a execução do progrma */
-void error(const char *msg) {
-	perror(msg);
-	exit(EXIT_FAILURE);
-}
-
